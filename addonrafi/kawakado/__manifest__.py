4c16b8ed3176e6a9b034ef8fd6eb8b4edf2f4e32
Muhammad Rafi Sofyan <muhammad.sofyan@hashmicro.id>
2022-09-12 09:05:05 +0700

    add models to security and implement VA menu

--- addonrafi/kawakado/__manifest__.py
+++ addonrafi/kawakado/__manifest__.py
@@ -23,9 +23,11 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu.xml',
+        'views/voiceactor_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
