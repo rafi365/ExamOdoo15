029e5b51538cd228fe8ba8b658373e7f8f9288ed
Muhammad Rafi Sofyan <muhammad.sofyan@hashmicro.id>
2022-09-12 17:15:04 +0700

    add gender to VA

--- addonrafi/kawakado/models/voiceactor.py
+++ addonrafi/kawakado/models/voiceactor.py
@@ -6,6 +6,7 @@ class VoiceActor(models.Model):
     _description = 'voiceactor class'
 
     name = fields.Char(string='VA Name',required=True)
+    gender = fields.Selection(string='Gender', selection=[('male', 'Male'), ('female', 'Female'),],required=True)
     desc = fields.Char(string='Description')
     character_ids = fields.One2many('kawakado.character', 'va_id', string='character',readonly=True)
 
