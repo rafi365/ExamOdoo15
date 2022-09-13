0597b5f69af19311cebfa3778d1ee3eac0d17377
Muhammad Rafi Sofyan <muhammad.sofyan@hashmicro.id>
2022-09-13 13:27:21 +0700

    implement item sold on create edit delete

--- addonrafi/kawakado/models/transaction.py
+++ addonrafi/kawakado/models/transaction.py
@@ -13,6 +13,43 @@ class Transaction(models.Model):
     company_ids = fields.Many2many('kawakado.company', string='Buying Company')
     transaction_ids = fields.One2many('kawakado.transaction_details', 'transaction_id', string='Transaction')
 
+    def write(self, vals):
+        for i in self:
+            for line in i:
+                penjualan = i.env['kawakado.transaction_details'].search(
+                    [('transaction_id', '=', line.id)])
+
+            for ob in penjualan:
+                ob.name.items_sold -= ob.amount
+      
+        line = super(Transaction, self).write(vals)
+        
+        for line in self:
+            data_setelah_edit = self.env['kawakado.transaction_details'].search(
+                    [('transaction_id', '=', line.id)])
+            for data_baru in data_setelah_edit:
+                if data_baru in penjualan:
+                    data_baru.name.items_sold += data_baru.amount
+                else:
+                    pass
+
+        return line
+
+    def unlink(self):
+        for i in self:
+            if i.transaction_ids:
+                penjualan = []
+                for line in i:
+                    penjualan = i.env['kawakado.transaction_details'].search(
+                        [('transaction_id', '=', line.id)])
+
+                for ob in penjualan:
+                    ob.name.items_sold -= ob.amount
+
+        line = super(Transaction, self).unlink()
+        return line
+    
+
 class TransactionDetails(models.Model):
     _name = 'kawakado.transaction_details'
     _description = 'detail of transaction'
@@ -20,4 +57,14 @@ class TransactionDetails(models.Model):
     name = fields.Many2one('kawakado.published', string='Title')
     transaction_id = fields.Many2one('kawakado.transaction', string='Transaction')
     amount = fields.Integer(string='Amount')
+    @api.model
+    def create(self, vals):
+        line = super(TransactionDetails, self).create(vals)
+        if line.amount:
+            # Mendapatkan data berdasarkan ID pada barang_id
+            self.env['kawakado.published'].search(
+                [('id', '=', line.name.id)]
+            ).write({'items_sold': line.name.items_sold + line.amount})
+        return line
+    
     
