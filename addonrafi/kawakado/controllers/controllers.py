4e593c20e9375f95818b1daa6efc3bbda18cb7ec
Muhammad Rafi Sofyan <muhammad.sofyan@hashmicro.id>
2022-09-13 12:21:23 +0700

    implement list catalogue API

--- addonrafi/kawakado/controllers/controllers.py
+++ addonrafi/kawakado/controllers/controllers.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 # from odoo import http
+from odoo import http,models,fields
+from odoo.http import request
+import json
+from datetime import timedelta
+class Kawakado(http.Controller):
+    @http.route('/kawakado/listcatalogue', auth='public',method=['GET'])
+    def getCatalogue(self, **kw):
+        publ = request.env['kawakado.published'].search([])
+        isi = []
+        for bb in publ:
+            isi.append({
+                'Title' : bb.name,
+                'Type' : bb.works_type_id.name,
+                'status' : bb.status,
+                'publish_date':(bb.publish_date+ timedelta(hours=7)).strftime("%d/%m/%Y"),
+                'sold' : bb.items_sold,
+            })
+        return json.dumps(isi)
 
 
 # class Kawakado(http.Controller):
