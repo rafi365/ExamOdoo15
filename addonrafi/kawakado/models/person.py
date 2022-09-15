3e35d521556d407acd3355df8d4466296d5e98e2
Muhammad Rafi Sofyan <muhammad.sofyan@hashmicro.id>
2022-09-15 14:44:40 +0700

    fix onchange and add a compute variant

--- addonrafi/kawakado/models/person.py
+++ addonrafi/kawakado/models/person.py
@@ -8,11 +8,18 @@ class Person(models.Model):
     name = fields.Char(string='Name')
     alamat = fields.Char(string='Address')
     tgl_lahir = fields.Datetime(string='BirthDate')
-    useless = fields.Char(string=' ')
+    useless = fields.Char(string='onchange')
+    uselessv2 = fields.Char(string='compute',compute="_oncomp")
 
     @api.onchange('name')
-    def onnamechange(self):
-        self.useless = self.name
+    def _onnamechange(self):
+        for i in self:
+            i.useless = i.name
+
+    @api.depends('name')
+    def _oncomp(self):
+        for i in self:
+            i.uselessv2 = i.name
     
 
 
